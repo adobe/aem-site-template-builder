77708883dc92e2b0e2b8e5a409fc8b62d68d7886
Bartosz Glowacki <bartosz.glow@gmail.com>
2020-10-19 11:24:53 +0200

    zip package fix

--- bin/index.js
+++ bin/index.js
@@ -5,6 +5,8 @@ const path = require('path');
 const yargs = require('yargs/yargs');
 const shell = require('shelljs');
 const clientlib = require("aem-clientlib-generator");
+const zip = require('bestzip');
+const colors = require('colors/safe');
 
 const terminal = require('../lib/terminal');
 const PATHS = require('../lib/config').paths;
@@ -41,34 +43,40 @@ shell.cd('..');
 shell.echo(terminal.prefix, 'Creating clientlibs...');
 clientlib(clientlibConfig.libs, { verbose: true }, function () {
   shell.echo(terminal.prefix, 'Clientlibs properly generated!');
+
+  // Build content package
+  shell.echo(terminal.prefix, 'Building content package...');
+  shell.cd('site.template');
+  shell.exec('mvn clean install');
+  shell.cd('..');
+
+  // Prepare site template folder
+  shell.echo(terminal.prefix, 'Preparing Site Template structure...');
+  shell.rm('-rf', PATHS.siteTemplate);
+  shell.rm('-rf', `${PATHS.siteTemplate}.zip`);
+  shell.mkdir('-p', PATHS.siteTemplate);
+
+  // Copy all files into folder
+  shell.cp('-r', [PATHS.files, PATHS.previews, PATHS.properties, 'site.template/target/*.zip' ], PATHS.siteTemplate);
+
+  // Zip theme sources
+  shell.echo(terminal.prefix, 'Zipping Theme sources...');
+  shell.cd('site.theme');
+  shell.exec(`zip ../${PATHS.siteTemplate}/theme-sources.zip $(git ls-files)`);
+  shell.cd('..');
+
+  // Zip Site Template package
+  shell.echo(terminal.prefix, 'Zipping Site Template package...');
+
+  zip({
+    source: `*`,
+    cwd: PATHS.siteTemplate,
+    destination: path.join(process.cwd(), `../${PATHS.siteTemplate}.zip`)
+  }).then(function() {
+    shell.rm('-rf', 'site-template');
+    shell.echo(terminal.prefix, `Site Template package: ${PATHS.siteTemplate}.zip`);
+  }).catch(function(err) {
+    shell.echo(colors.error(`[${terminal.packageName}]: Failed to zip Site Template package: ${err.stack}`));
+    process.exit(1);
+  });
 });
-
-// Build content package
-shell.echo(terminal.prefix, 'Building content package...');
-shell.cd('site.template');
-shell.exec('mvn clean install');
-shell.cd('..');
-
-// Prepare site template folder
-shell.echo(terminal.prefix, 'Preparing Site Template structure...');
-shell.rm('-rf', PATHS.siteTemplate);
-shell.rm('-rf', `${PATHS.siteTemplate}.zip`);
-shell.mkdir('-p', PATHS.siteTemplate);
-
-// Copy all files into folder
-shell.cp('-r', [PATHS.files, PATHS.previews, PATHS.properties, 'site.template/target/*.zip' ], PATHS.siteTemplate);
-
-// Zip theme sources
-shell.echo(terminal.prefix, 'Zipping Theme sources...');
-shell.cd('site.theme');
-shell.exec(`zip ../${PATHS.siteTemplate}/theme-sources.zip $(git ls-files)`);
-shell.cd('..');
-
-// Zip Site Template package
-shell.echo(terminal.prefix, 'Zipping Site Template package...');
-shell.cd(PATHS.siteTemplate);
-shell.exec(`bestzip ../${PATHS.siteTemplate}.zip *`);
-shell.cd('..');
-
-// Clear site template folder
-shell.rm('-rf', 'site-template');
