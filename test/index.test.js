4a08f7e19785e81e530006fb0dd0041f23a4f076
Tobias Reiss <tobi+github@basecode.de>
2020-10-29 17:49:45 +0100

    nicer tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,15 +1,22 @@
 const test = require('ava');
 const path = require('path');
 
-test('Create site template artifact', async t => {
-    const cwd = process.cwd();
-    process.chdir(path.join(cwd, 'test/resources/some-site-template'));
-    
-    require(path.join(cwd, 'bin/index.js'));
-    
-    // sleep for a few js cycles to await the childprocess in index.js
+const TEST_TEMPLATE_FOLDER_PATH = path.join(process.cwd(), 'test/resources/some-site-template');
+const UNDER_TEST_PATH = path.join(process.cwd(), 'bin/index.js');
+
+test.beforeEach(t => {
+    process.chdir(TEST_TEMPLATE_FOLDER_PATH);
+});
+
+test.afterEach.always( async t => {
+    // sleep for a few process cycles to await the childprocess in index.js
     await new Promise((resolve) => { setTimeout(resolve, 1000); });
 
-    process.chdir(cwd);
+    process.chdir(TEST_TEMPLATE_FOLDER_PATH);
+    delete require.cache[require.resolve(UNDER_TEST_PATH)];
+});
+
+test('Create site template artifact', async t => {
+    require(path.join(cwd, 'bin/index.js'));
     t.pass();
 });
