8df5960647fb47f770698115e88e18c645487974
Tobias Reiss <tobi+github@basecode.de>
2020-10-29 18:01:21 +0100

    feat: site template name includes version

--- test/index.test.js
+++ test/index.test.js
@@ -1,22 +1,24 @@
 const test = require('ava');
 const path = require('path');
+const fs = require('fs');
 
-const TEST_TEMPLATE_FOLDER_PATH = path.join(process.cwd(), 'test/resources/some-site-template');
-const UNDER_TEST_PATH = path.join(process.cwd(), 'bin/index.js');
+const ROOT_PATH = process.cwd();
+const TEST_TEMPLATE_FOLDER_PATH = path.join(ROOT_PATH, 'test/resources/some-site-template');
+const UNDER_TEST_PATH = path.join(ROOT_PATH, 'bin/index.js');
 
 test.beforeEach(t => {
     process.chdir(TEST_TEMPLATE_FOLDER_PATH);
 });
 
 test.afterEach.always( async t => {
-    // sleep for a few process cycles to await the childprocess in index.js
-    await new Promise((resolve) => { setTimeout(resolve, 1000); });
-
-    process.chdir(TEST_TEMPLATE_FOLDER_PATH);
+    process.chdir(ROOT_PATH);
     delete require.cache[require.resolve(UNDER_TEST_PATH)];
 });
 
 test('Create site template artifact', async t => {
-    require(path.join(cwd, 'bin/index.js'));
-    t.pass();
+    require(UNDER_TEST_PATH);
+
+    // sleep for a few process cycles to await the childprocess in index.js
+    await new Promise((resolve) => { setTimeout(resolve, 1000); });
+    t.true(fs.existsSync(path.join(TEST_TEMPLATE_FOLDER_PATH, 'some-site-template-1.0.0.zip')));
 });
